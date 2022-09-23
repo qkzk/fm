e9a68af38431805410dbe7ab95921c0e8bb9fa63
quentin konieczko <konieczko@gmail.com>
2022-09-23 11:20:16 +0200

    clippy

--- src/fileinfo.rs
+++ src/fileinfo.rs
@@ -161,7 +161,7 @@ fn is_not_hidden(entry: &DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
-        .map(|s| !s.starts_with("."))
+        .map(|s| !s.starts_with('.'))
         .unwrap_or(false)
 }
 fn extract_datetime(direntry: &DirEntry) -> String {
