47686209158824bee1aa3c48a82a112818209166
qkzk <qu3nt1n@gmail.com>
2022-11-07 14:10:03 +0100

    git: origin of source code

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copied from https://github.com/9ary/gitprompt-rs/blob/master/src/main.rs
+// Couldn't use without forking and I'm lazy.
 
 use std::error::Error;
 use std::fmt::Write as _;
