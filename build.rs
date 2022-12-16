53f06729d62abe362c38ccacf12a9b2b4e11c578
qkzk <qu3nt1n@gmail.com>
2022-12-16 21:55:16 +0100

    improve build.rs

--- build.rs
+++ build.rs
@@ -3,17 +3,21 @@ use std::borrow::Borrow;
 /// Creates a config folder for fm with default config files.
 /// The source is `config_files/fm`.
 /// The destination is `~/.config/fm`.
+/// If there's already some configuration files, no overwrite is done.
 fn main() {
-    let mut default_config_files = std::env::current_dir().unwrap();
-    default_config_files.push("config_files/fm");
+    if let Ok(mut default_config_files) = std::env::current_dir() {
+        default_config_files.push("config_files/fm");
 
-    let config_folder_cow = shellexpand::tilde("~/.config");
-    let config_folder: &str = &config_folder_cow.borrow();
+        let config_folder_cow = shellexpand::tilde("~/.config");
+        let config_folder: &str = config_folder_cow.borrow();
+        let mut copy_options = fs_extra::dir::CopyOptions::new();
+        copy_options.skip_exist = true;
 
-    fs_extra::dir::copy(
-        default_config_files,
-        config_folder,
-        &fs_extra::dir::CopyOptions::new(),
-    )
-    .unwrap();
+        match fs_extra::dir::copy(default_config_files, config_folder, &copy_options) {
+            Ok(_) => (),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    } else {
+        eprintln!("Environment variable $PWD should be set. Couldn't find the source folder.")
+    }
 }
