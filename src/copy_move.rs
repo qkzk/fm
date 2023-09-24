fb87fe0068109143df23c671ba8bf50fc3f9bae3
qkzk <qu3nt1n@gmail.com>
2023-09-24 19:26:27 +0200

    refactor copy_move (a little bit). Use const instead of static for &str

--- src/copy_move.rs
+++ src/copy_move.rs
@@ -9,14 +9,15 @@ use indicatif::{InMemoryTerm, ProgressBar, ProgressDrawTarget, ProgressState, Pr
 use log::info;
 use tuikit::prelude::{Attr, Color, Effect, Event, Term};
 
+use crate::constant_strings_paths::NOTIFY_EXECUTABLE;
 use crate::fileinfo::human_size;
 use crate::opener::execute_in_child;
 
-fn setup(
+fn setup_progress_bar(
     action: String,
-    height: usize,
-    width: usize,
+    size: (usize, usize),
 ) -> Result<(InMemoryTerm, ProgressBar, fs_extra::dir::CopyOptions)> {
+    let (height, width) = size;
     let in_mem = InMemoryTerm::new(height as u16, width as u16);
     let pb = ProgressBar::with_draw_target(
         Some(100),
@@ -47,16 +48,7 @@ fn handle_progress_display(
     process_info: fs_extra::TransitProcess,
 ) -> fs_extra::dir::TransitProcessResult {
     pb.set_position(progress_bar_position(&process_info));
-    let _ = term.print_with_attr(
-        1,
-        0,
-        &in_mem.to_owned().contents(),
-        Attr {
-            fg: Color::CYAN,
-            bg: Color::default(),
-            effect: Effect::REVERSE | Effect::BOLD,
-        },
-    );
+    let _ = term.print_with_attr(1, 0, &in_mem.to_owned().contents(), CopyMove::attr());
     let _ = term.present();
     fs_extra::dir::TransitProcessResult::ContinueOrAbort
 }
@@ -79,6 +71,14 @@ pub enum CopyMove {
 }
 
 impl CopyMove {
+    fn attr() -> Attr {
+        Attr {
+            fg: Color::CYAN,
+            bg: Color::Default,
+            effect: Effect::REVERSE | Effect::BOLD,
+        }
+    }
+
     fn verb(&self) -> &str {
         match *self {
             CopyMove::Copy => "copy",
@@ -105,49 +105,65 @@ pub fn copy_move(
     term: Arc<Term>,
 ) -> Result<()> {
     let c_term = term.clone();
-    let (height, width) = term.term_size()?;
-    let (in_mem, pb, options) = setup(copy_or_move.verb().to_owned(), height, width)?;
+    let (in_mem, pb, options) =
+        setup_progress_bar(copy_or_move.verb().to_owned(), term.term_size()?)?;
     let handle_progress = move |process_info: fs_extra::TransitProcess| {
         handle_progress_display(&in_mem, &pb, &term, process_info)
     };
     let dest = dest.to_owned();
     let _ = thread::spawn(move || {
-        let copier_mover = match copy_or_move {
-            CopyMove::Copy => fs_extra::copy_items_with_progress,
-            CopyMove::Move => fs_extra::move_items_with_progress,
-        };
+        let copier_mover = pick_copy_or_move(&copy_or_move);
         let transfered_bytes = match copier_mover(&sources, &dest, &options, handle_progress) {
             Ok(transfered_bytes) => transfered_bytes,
             Err(e) => {
-                info!("copy move couldn't copy: {:?}", e);
+                info!("copy move couldn't copy: {e:?}");
                 0
             }
         };
 
         let _ = c_term.send_event(Event::User(()));
-        let _ = notify(&format!(
-            "fm: {} finished {}B {}",
+
+        inform_of_copy(
             copy_or_move.verb(),
             human_size(transfered_bytes),
-            copy_or_move.preterit()
-        ));
-        info!(
-            "{} finished {}B",
-            copy_or_move.verb(),
-            human_size(transfered_bytes)
+            copy_or_move.preterit(),
         );
-        info!(target: "special",
-            "{} finished {}B",
-            copy_or_move.verb(),
-            human_size(transfered_bytes)
-        )
     });
     Ok(())
+}
+
+fn pick_copy_or_move<P, Q, F>(
+    copy_or_move: &CopyMove,
+) -> for<'a, 'b> fn(
+    &'a [P],
+    Q,
+    &'b fs_extra::dir::CopyOptions,
+    F,
+) -> Result<u64, fs_extra::error::Error>
+where
+    P: AsRef<std::path::Path>,
+    Q: AsRef<std::path::Path>,
+    F: FnMut(fs_extra::TransitProcess) -> fs_extra::dir::TransitProcessResult,
+{
+    match copy_or_move {
+        CopyMove::Copy => fs_extra::copy_items_with_progress,
+        CopyMove::Move => fs_extra::move_items_with_progress,
+    }
+}
+
+fn inform_of_copy(verb: &str, hs_bytes: String, preterit: &str) {
+    let _ = notify(&format!("fm: {} finished {}B {}", verb, hs_bytes, preterit));
+    info!("{} finished {}B", verb, hs_bytes,);
+    info!(target: "special",
+        "{} finished {}B",
+        verb,
+        hs_bytes,
+    )
 }
 
 /// Send a notification to the desktop.
 /// Requires "notify-send" to be installed.
 fn notify(text: &str) -> Result<()> {
-    execute_in_child("notify-send", &[text])?;
+    execute_in_child(NOTIFY_EXECUTABLE, &[text])?;
     Ok(())
 }
