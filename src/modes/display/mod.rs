646c92e6cdd4ab22a40632893123e3f117ff719c
qkzk <qu3nt1n@gmail.com>
2023-12-02 16:49:05 +0100

    renamed lsl to directory

--- src/modes/display/mod.rs
+++ src/modes/display/mod.rs
@@ -1,17 +1,17 @@
 mod content_window;
+mod directory;
 mod fileinfo;
-mod lsl;
 mod preview;
 mod skim;
 mod tree;
 mod users;
 
 pub use content_window::ContentWindow;
+pub use directory::{files_collection, human_size, read_symlink_dest, shorten_path, Directory};
 pub use fileinfo::{
     convert_octal_mode, extract_extension, fileinfo_attr, is_not_hidden, ColorEffect, FileInfo,
     FileKind,
 };
-pub use lsl::{files_collection, human_size, read_symlink_dest, shorten_path, Directory};
 pub use preview::{ColoredTriplet, ExtensionKind, MakeTriplet, Preview, TextKind, Window};
 pub use skim::{print_ansi_str, Skimer};
 pub use tree::{calculate_top_bottom, ColoredString, Go, Node, To, Tree};
