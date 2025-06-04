6a2e940eeb7cde80b0c17d6fc7b09ade56057479
qkzk <qu3nt1n@gmail.com>
2025-06-04 22:27:45 +0200

    remove comment

--- src/modes/display/mod.rs
+++ src/modes/display/mod.rs
@@ -1,17 +1,14 @@
 mod directory;
-mod preview;
-// TODO! skim...
-// mod skim;
 mod nucleo_picker;
+mod preview;
 mod tree;
 mod uber;
 
 pub use directory::{files_collection, human_size, Directory};
+pub use nucleo_picker::{highlighted_text, parse_line_output, Direction, FuzzyFinder, FuzzyKind};
 pub use preview::{
     BinaryContent, ExtensionKind, HLContent, Line as BinLine, Preview, PreviewBuilder, TakeSkip,
     TakeSkipEnum, Text, TextKind,
 };
-// pub use skim::{parse_line_output, print_ansiq_str, Skimer};
-pub use nucleo_picker::{highlighted_text, parse_line_output, Direction, FuzzyFinder, FuzzyKind};
 pub use tree::{Go, Node, TLine, To, Tree, TreeBuilder, TreeLines};
 pub use uber::{path_is_video, Thumbnail, Ueber, UeberBuilder};
